19ed79568bf51316af4b08abb0b651799be069d5
Reza <rezabaiust@gmail.com>
2023-01-02 03:36:09 +0600

    Authentication

---
+++ README.md
@@ -0,0 +1,11 @@
+## Setup & Installtion
+Make sure you have the latest version of Python installed.
+
+### Install Dependencies
+```cmd
+pip install -r requirements.txt
+```
+### Run the application
+```cmd
+python main.py
+```
--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,14 +1,31 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
-
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('User not found with this email.', category='error')
+    return render_template('auth/login.html', user=current_user)
 
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         name = request.form.get('name')
@@ -16,17 +33,33 @@ def register():
         password = request.form.get('password')
         confirm_password = request.form.get('confirm_password')
 
-        if password != confirm_password:
+        user = User.query.filter_by(email=email).first()
+        if user:
+                flash('Email already exists.', category='error')
+        elif len(name) < 2:
+            flash('Name must be greater than 1 character.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 character.', category='error')
+        elif password != confirm_password:
             flash('Password not match!', category='error')
         else:
-            flash('Account created!')
+            new_user = User(name=name, email=email, password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
+            flash('Account created successfully!', category='success')
+            return redirect(url_for('views.home'))
 
-    return render_template('auth/register.html')
+    return render_template('auth/register.html', user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "Logout"
+    return redirect(url_for('auth.login'))
+
+
+
 
 
 
